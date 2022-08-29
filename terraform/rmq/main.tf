f8462098634b8dc9d97c7bc88839a61ed82dee3b
josueSarabia <josuedavids@hotmail.com>
2022-08-28 21:17:25 -0500

    first commit - setup terraform

---
+++ terraform/rmq/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region="us-east-1"
+    profile=var.profile
+}
+
+resource "aws_instance" "rmq" {
+    ami = "ami-052efd3df9dad4825"
+    instance_type = "t2.micro"
+    key_name = "devops"
+    vpc_security_group_ids = [ "sg-0dee4f6172f74082f" ]
+
+    tags = {
+        Name = var.name
+        Group = var.group
+    }
+
+}
